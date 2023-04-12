8dc607bc47a204726f3244909fd01e9e8a569e40
abhinavverma-tracencode <abhinav@tracencode.tech>
2023-04-12 13:28:03 +0530

    first commit register

---
+++ web_apis/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'WEBSITE APIS ',
+    'version': '1.2',
+    'category': 'WEBSITE APIS PRODUCT',
+    'summary': 'Website  & Apis Availability  Website and Product',
+    'description': """
+    This module contains all the common features of Website  & Apis Availability""",
+    'depends': ['website'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/web_view.xml',
+        'views/register_views.xml',
+        'views/website_form.xml',
+        ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install':False,
+}
